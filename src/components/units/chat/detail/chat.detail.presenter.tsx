b2b1289dfebf5fcc20a4c963a60b861a2ac55c40
gilgyujenong <jgg7645@naver.com>
2024-10-21 19:03:05 +0900

    Feat: 채팅대화방 레이아웃

--- src/components/units/chat/detail/chat.detail.presenter.tsx
+++ src/components/units/chat/detail/chat.detail.presenter.tsx
@@ -3,7 +3,18 @@ import * as S from './chat.detail.styles'
 export default function ChatDetailPresenter() {
     return (
         <S.MainBox>
-            채팅 상세 페이지
+            <S.WriteBox>
+                <S.WriteInput placeholder='메시지 입력' rows={4} />
+                <S.WriteInputBox>
+                    <S.IconBox>
+                        <S.locationButton />
+                        <S.ImageButton />
+                        <S.EmoticonButton />
+                    </S.IconBox>
+                    <S.WriteButton>전송</S.WriteButton>
+                </S.WriteInputBox>
+                
+            </S.WriteBox>
         </S.MainBox>
     )
 }
